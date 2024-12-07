c35d9beafccfa035b03f7d51db37202f3c866f8d
kaye <jhan117@naver.com>
2024-12-07 22:36:09 +0900

    연결 사이트 링크 수정

--- src/utils/gameData.js
+++ src/utils/gameData.js
@@ -4,21 +4,21 @@ const gamesData = [
     developer: "Riot Games",
     genre: ["FPS", "액션", "히어로 슈팅"],
     releaseDate: "2020-06-02",
-    website: "https://playvalorant.com/",
+    website: "https://playvalorant.com/ko-kr/",
   },
   {
     name: "Rainbow Six Siege",
     developer: "Ubisoft Montreal",
     genre: ["히어로 슈팅", "전술 FPS"],
     releaseDate: "2015-12-01",
-    website: "https://www.ubisoft.com/game/rainbow-six/siege",
+    website: "https://www.ubisoft.com/ko-kr/game/rainbow-six/siege",
   },
   {
     name: "Minecraft",
     developer: "Mojang Studios",
     genre: ["샌드박스", "서바이벌", "판타지"],
     releaseDate: "2011-11-18",
-    website: "https://www.minecraft.net/",
+    website: "https://www.minecraft.net/ko-kr",
   },
 ];
 
