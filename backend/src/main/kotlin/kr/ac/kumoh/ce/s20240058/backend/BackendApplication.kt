ecdca4bd43f2d69cf9a8e6996396557c2f368dce
kaye <jhan117@naver.com>
2024-12-17 15:19:18 +0900

    backend 추가

---
+++ backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20240058.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/kr/ac/kumoh/ce/s20240058/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20240058.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
