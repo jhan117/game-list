27de94d437aa9bb3b370c0ead169e865bd59ae17
kaye <jhan117@naver.com>
2024-12-18 14:29:19 +0900

    cors 수정

--- backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/WebConfig.kt
+++ backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/WebConfig.kt
@@ -1,6 +1,7 @@
 package kr.ac.kumoh.ce.s20240058.backend
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.*
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -8,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://kaye-game.netlify.app")
-            .allowedMethods("GET", "OPTIONS")
+            .allowedOrigins("http://localhost:3000", "https://kaye-game.netlify.app/")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
     }
