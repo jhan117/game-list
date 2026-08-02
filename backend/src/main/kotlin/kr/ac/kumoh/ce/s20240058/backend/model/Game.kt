b781a421ecb7e4d1f186d4925c70183e8d9c7612
kaye <jhan117@naver.com>
2026-08-02 23:54:03 +0900

    fix(backend): make model fields nullable and specify mongodb database name

--- backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/model/Game.kt
+++ backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/model/Game.kt
@@ -1,21 +1,23 @@
 package kr.ac.kumoh.ce.s20240058.backend.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Document(collection = "games")
 data class Game(
     @Id
     val id: String? = null,
-    val name: String,
-    val genre: String,
-    val developer: String,
-    val genres: List<String>,
-    val releaseDate: String,
-    val website: String,
-    val interestLevel: Int,
-    val reasonForInterest: String,
+    val name: String? = null,
+    val genre: String? = null,
+    val developer: String? = null,
+    val genres: List<String>? = null,
+    val releaseDate: String? = null,
+    val website: String? = null,
+    val interestLevel: Int? = null,
+    val reasonForInterest: String? = null,
 
     @Field("gifData")
     var gifData: GifData? = null,
--- backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/model/GifData.kt
+++ backend/src/main/kotlin/kr/ac/kumoh/ce/s20240058/backend/model/GifData.kt
@@ -1,11 +1,14 @@
 package kr.ac.kumoh.ce.s20240058.backend.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.data.mongodb.core.mapping.Document
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Document(collection = "gifData")
 data class GifData(
-    val name: String,
-    val altText: String,
-    val gifUrl: String,
-    val hoverImgUrl: String,
+    val id: String? = null,
+    val name: String? = null,
+    val altText: String? = null,
+    val gifUrl: String? = null,
+    val hoverImgUrl: String? = null,
 )
